28aa74bd910911c2d073bf80b6f5505dc5148cd7
Roger <roger@getvobot.com>
2019-04-06 17:33:39 +0800

    feat(weather): initial weather station example

---
+++ weather/README.md
@@ -0,0 +1,16 @@
+# Weathers Example
+
+This is a simple example of weather station. You may create a personalised fridge magnet with this app.
+
+Let's say we want to update the display with latest weather forecast every hour, this app will do these to archive this goal:
+
+- Request for latest weather over HTTP GET every hour
+- Analysis the weather condition
+- Generate a layout template and send to wireless display
+
+## SETUP
+
+- Sign-up a free account and obtain an API Key from [OpenWeatherMap](https://home.openweathermap.org/api_keys)
+- Replace the placeholder of OPENWEATHERMAP_APIKEY with your API key
+- Edit CITY_NAME & COUNTRY_CODE
+
---
+++ weather/app/__init__.py
@@ -0,0 +1,130 @@
+import logging
+import uasyncio as asyncio
+import arequests as requests
+import asyn
+import json
+
+# Change to your own settings
+OPENWEATHERMAP_APIKEY = 'REPLACE_THIS_WITH_YOUR_OWN_API_KEY'
+CITY_NAME = 'Los Angeles'
+COUNTRY_CODE = 'us'
+
+# Options
+REFRESH_INTERVAL = 60 * 60 # refresh every hour
+LAYOUT_FILE = "/app/layout_4in2.json"
+
+class App:
+    """App for periodic update of weather station
+
+    - Request for latest weather over HTTP GET every hour
+    - Analysis the weather condition
+    - Generate a layout template and send to wireless display
+    """
+
+    def __init__(self, loop, pan):
+        self.log = logging.getLogger("APP")
+        self.log.setLevel(logging.DEBUG)
+        self.log.info('App Starting')
+        self.pan = pan
+        self.targetNodeId = None
+        self.nodeOnlineEvent = asyn.Event()
+
+    async def task(self):
+        """Main task of App Class
+        This coro task() will be brought up automatically by system.
+        """
+        # waiting for at least one display node becomes online
+        await self.nodeOnlineEvent
+        self.targetNodeId = self.nodeOnlineEvent.value()
+        self.nodeOnlineEvent.clear()
+
+        # loop forever
+        while True:
+            self.log.debug('App Task Running')
+            await self._fetchLastestWeather()
+            await asyncio.sleep(REFRESH_INTERVAL) # sleep for a while
+
+    def onNodePresence(self, nodeId, isOnline):
+        """Event of presence/absence
+        Whenever a node becomes online or offline, this method will be called by system
+        Args:
+            nodeId: (int) indicates which node is changing its status
+            isOnline: (bool) =True, a presence; =False: absence
+        """
+        self.log.info('node "%s" is %s', hex(nodeId), 'online.' if isOnline else 'offline!')
+        if isOnline:
+            self.nodeOnlineEvent.set(nodeId)
+
+    async def _fetchLastestWeather(self):
+        """Lookup current weather info through a REST API request"""
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={cityName},{countryCode}&appid={apiKey}'.format(
+                cityName = CITY_NAME, countryCode = COUNTRY_CODE, apiKey = OPENWEATHERMAP_APIKEY )
+        self.log.debug(url)
+        response = None
+        try:
+            response = await requests.get(url)
+            res = await response.json()
+            if not 'cod' in res:
+                self.log.error(res)
+            else:
+                code = res['cod']
+                if code == 200:
+                    await self._parseWeather(res)
+                else:
+                    self.log.error('invalid response: {}'.format(code))
+        except Exception as e:
+            self.log.exception(e, 'unable to get weather')
+            if response:
+                self.log.debug(str(response._cached))
+        finally:
+            if response: # If the error is an OSError the socket has to be closed.
+                await response.close()
+
+    async def _parseWeather(self, response):
+        """Extract weather details from response"""
+        try:
+            weather = response['weather']
+            main = response['main']
+            self.log.info('Weather: {} {}'.format(weather, main))
+            await self._sendToDisplay(weather[0]['main'], main['temp'], main['humidity'])
+        except Exception as e:
+            self.log.exception(e, 'unable to parse weather')
+
+    async def _sendToDisplay(self, mainCondition, temperatureKelvin, humidity):
+        """Create layout render template and send to display"""
+        if not self.pan:
+            return False
+        self.log.debug('send to display')
+        try:
+            with open(LAYOUT_FILE, 'r') as f:
+                layout = json.loads(f.read())
+
+                import core.device_display as disp
+                dp = disp.getSpec(disp.DISPLAY_4IN2_BW)
+                fullWidth = dp['resolution']['width']
+                from core.font_render import FontRender
+                self.font = FontRender()
+
+                # Replace weather condition data in layout
+                width = self.font.getRenderedWidth(mainCondition, self.font.FONT_YKSZ_BOLD_44) # calculate width, in order to align center
+                layout['items'][0]['data']['caption'] = mainCondition
+                layout['items'][0]['data']['offset']['x'] = (fullWidth - width - 48 * 2) // 2
+                # Replace temperature & humidity data in layout
+                fahrenheit = int((temperatureKelvin * 9 / 5 - 459.67) * 10) / 10
+                # celsius = int((temperatureKelvin - 273.15) * 10) / 10
+                temp = str(fahrenheit) + "\'F  " + str(humidity) + "%"
+                width = self.font.getRenderedWidth(temp, self.font.FONT_DIN_CON_32) # calculate width, in order to align center
+                layout['items'][1]['data']['caption'] = temp
+                layout['items'][1]['data']['offset']['x'] = (fullWidth - width - 48 * 2) // 2
+
+                """Send to display"""
+                '''
+                # if you have more than one nodes, try lookup like this:
+                nodes = self.pan.getNodes()
+                if len(nodes):
+                    self.targetNodeId = nodes[0]
+                '''
+                return await self.pan.updateDisplay(self.targetNodeId, layout)
+        except Exception as e:
+            self.log.exception(e, 'unable to process layout')
+        return False
