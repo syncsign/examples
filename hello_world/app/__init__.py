6c687e9e378fb30b42d686063459fa37494bb6ca
Roger <roger@getvobot.com>
2019-06-02 13:33:52 +0800

    fix(hello): simplified

--- hello_world/app/__init__.py
+++ hello_world/app/__init__.py
@@ -1,46 +1,32 @@
 import uasyncio as asyncio
 import json
-from core.constants import *
+
+TEMPLATE = '''
+{
+    "items": [
+        { "type": "TEXT",
+          "data": {
+            "text": "HELLO WORLD",
+            "block": { "x": 120, "y": 110, "w": 200, "h": 56 }
+          }
+        }
+    ]
+}
+'''
 
 class App:
     # User App of Hub SDK, send a 'Hello world' to wireless display
 
     def __init__(self, mgr, loop, pan):
         self.pan = pan
-        loop.create_task(self.task()) # run a asyncio task
-        self.targetNodeId = None
-        mgr.setPanCallback(self.onPanEvent)
-
-    async def task(self):
-        # This coro task() was brought up by __init__()
-        # waiting for at least one display node becomes online
-        while self.targetNodeId is None:
-            await asyncio.sleep(1)
-        await self.printHello()
-
-    def onPanEvent(self, event, data):
-        # Event listener, handling NODE.PRESENCE and remember the nodeId
-        if event == EVT_NODE_PRESENCE:
-            if data['isOnline']:
-                self.targetNodeId = data['nodeId']
-                print('node:', self.targetNodeId, 'online.')
+        loop.create_task(self.printHello()) # run a asyncio coro task
 
     async def printHello(self):
+        # Wait for at least one display node becomes online
+        while 0 == len(self.pan.onlineNodes()):
+            await asyncio.sleep(1)
         # Create layout render template and send to display
-        try:
-            layout = '''
-            {
-                "items": [
-                    { "type": "TEXT",
-                      "data": {
-                        "caption": "HELLO WORLD",
-                        "block": { "x": 120, "y": 110, "w": 200, "h": 56 }
-                      }
-                    }
-                ]
-            }
-            '''
-            return await self.pan.updateDisplay(self.targetNodeId, json.loads(layout))
-        except Exception as e:
-            print('unable to process:', str(e))
-        return False
+        layout = json.loads(TEMPLATE)
+        # Select the first online node as the target
+        target = next(iter(self.pan.onlineNodes()))
+        return await self.pan.updateDisplay(target, layout)
